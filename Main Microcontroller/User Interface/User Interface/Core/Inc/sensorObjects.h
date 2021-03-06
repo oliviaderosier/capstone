3e34a6e55e0700302ead6fd22b4d336a7deee8a2
Austin Waldron <animal@live.ca>
2021-03-06 14:04:27 -0600

    commit to re-sync all my files

---
+++ Main Microcontroller/User Interface/User Interface/Core/Inc/sensorObjects.h
@@ -0,0 +1,43 @@
+/*
+ * sensorObjects.h
+ *
+ *  Created on: Jan 27, 2021
+ *      Author: colto
+ */
+
+#ifndef INC_SENSOROBJECTS_H_
+#define INC_SENSOROBJECTS_H_
+
+#include "main.h"
+
+//Function Prototypes
+void sendBattRequest(uint8_t);
+void sendTempRequest(uint8_t);
+
+void processATResponse(uint8_t*);
+void processIO(uint8_t*);
+
+void initializeNodes(void);
+
+uint8_t verifyChecksum(uint8_t*);
+uint8_t generateChecksum(uint8_t *frame);
+
+//Global Variables
+uint8_t uartBufferRX[50];
+uint8_t uartBufferTX[50];
+
+//Remote Command Options
+#define OPTION_APPLY	0x02 //apply changes
+
+struct sensorNode {
+	uint8_t address[8];
+
+	uint16_t resistive;
+	uint16_t capacative;
+	uint16_t battery;
+	uint16_t temperature;
+};
+
+struct sensorNode fairways[36];
+
+#endif /* INC_SENSOROBJECTS_H_ */
