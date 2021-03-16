687996ea7dc3240d7bd7d2ea19183b5d4bfb9c91
oliviaderosier <43077288+oliviaderosier@users.noreply.github.com>
2021-03-16 09:38:02 -0600

    Transmit file w APIs
    JS sending script to send sunset time and chance of rain. Encoded in base64 and formatted as chanceOfRainXXXsunsetTime, ex. 0XXX7:04.

--- Website/Transmit.js
+++ Website/Transmit.js
@@ -3,10 +3,44 @@
 
     (async _ => {
         try {
+
+            //rain API
+            let resp = await axios.get('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1e8c8e0551e4b798d3190230210202&q=Regina&num_of_days=1&format=json&tp=12&mca=no')
+            console.log(resp.data['data']['weather'][0]['hourly'][1]['chanceofrain']); 
+
+            var rain = resp.data['data']['weather'][0]['hourly'][1]['chanceofrain'];
+            
+            //set time API
+            let resp2 = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Regina&appid=6676b2af2b110e60dcd8e8d742d5ff8e&units=metric')
+            console.log(resp2.data['sys']['sunset'] * 1000);
+
+            //time conversion
+                var setTime = resp2.data['sys']['sunset'] * 1000;
+                var setDate = new Date(setTime);
+                setDate.toDateString();
+                var hours1 = setDate.getHours() - 12;
+                var minutes1 = setDate.getMinutes();
+
+                if(minutes1 < 10)
+                {
+                  var time1 = hours1 + ":0"+ minutes1;
+                }
+                else{
+                  var time1 = hours1 + ":"+ minutes1;
+                }
+            console.log(time1);
+
             let userpassword = "olivia.derosier@gmail.com:CAPSTONE!";
             let encodedAuth = Buffer.from(userpassword).toString('base64');
+            
+            //encoding data
+            let data = rain + "XXX" + time1;
+            
+            console.log(data);
+            
+            let encodedData = Buffer.from(data).toString('base64');
 
-            let data = "SGVsbG8sIFdvcmxkIQ==";
+            console.log(encodedData);
     
             const body = `
                 <sci_request version="1.0">
@@ -16,8 +50,8 @@
                         </targets>
                         <rci_request version="1.1">
                             <do_command target="xbgw">
-                                <send_serial addr="00:13:A2:00:41:68:0B:CA" encoding="base64">
-                                    ${data}
+                                <send_serial addr="00:13:A2:00:41:68:0B:D5" encoding="base64">
+                                    ${encodedData}
                                 </send_serial>
                             </do_command>
                         </rci_request>
